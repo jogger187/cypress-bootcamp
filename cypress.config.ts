6db1940de8a28d203bfd9a3799d8371901f2ea21
jaggerhsu <sohm187@gmail.com>
2023-02-24 11:27:29 +0800

    chore(cy.config): add baseUrl for page test

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,5 +5,10 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    /* 
+    If you have a front-end service in this project, plz set the baseUrl.
+    This will automatically prefix cy.visit() and cy.request() commands with this baseUrl.
+    */
+    // baseUrl: 'http://localhost:{port}', 
   },
 });
