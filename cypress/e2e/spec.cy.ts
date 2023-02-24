461a17fd1e9bb212cd9b1f81dbf61540dbf58029
jaggerhsu <sohm187@gmail.com>
2023-02-24 12:21:14 +0800

    chore(spec): modify spec for IG login

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,9 +1,9 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit(' http://122.116.251.52')
-    cy.get(':nth-child(1) > .input_osg').type('fake@email.com')
-    cy.get(':nth-child(1) > .input_osg').should('have.value', 'fake@email.com')
-    cy.get('.button_osg').click()
-
+    cy.visit('https://www.instagram.com/accounts/login/?source=desktop_nav&next=https%3A%2F%2Fwww.instagram.com%2Flogin%2F%3F__coig_login%3D1')
+    cy.get(':nth-child(1) > .x1i10hfl > ._acan > ._aacl').click()
+    cy.get(':nth-child(1) > .x1npaq5j > ._aa48 > ._aa4b').type('testman@gmail.com').should('have.value', 'testman@gmail.com')
+    cy.get(':nth-child(2) > .x1npaq5j > ._aa48 > ._aa4b').type('testman123').should('have.value', 'testman123')
+    cy.get('._abc2 > :nth-child(3)').click()
   })
 })
